cf079e8eba7990e6ac74e359047b89c4f659605b
Lofi Dewanto <blasiuslofi.dewanto@devk.de>
2024-04-05 19:29:00 +0200

    update apps

--- viola-devk-client/src/main/java/com/github/lofidewanto/viodevk/client/AppEntryPoint.java
+++ viola-devk-client/src/main/java/com/github/lofidewanto/viodevk/client/AppEntryPoint.java
@@ -2,23 +2,39 @@ package com.github.lofidewanto.viodevk.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.Window;
-import elemental2.dom.DomGlobal;
-import elemental2.dom.Event;
-import elemental2.dom.HTMLButtonElement;
+import elemental2.dom.*;
 
 import java.util.logging.Logger;
 
 public class AppEntryPoint implements EntryPoint {
 
-	private static final Logger logger = Logger
-			.getLogger(AppEntryPoint.class.getName());
+    private static final Logger logger = Logger
+            .getLogger(AppEntryPoint.class.getName());
 
-	@Override
-	public void onModuleLoad() {
-		final HTMLButtonElement button = (HTMLButtonElement) DomGlobal.document.getElementById("helloButton");
-		button.addEventListener("click", (Event event) -> {
-            Window.alert("Hello World Lofi!");
-			logger.info("Hello World Lofi!");
+    @Override
+    public void onModuleLoad() {
+        addButton();
+        addTextArea();
+    }
+
+     void addTextArea() {
+        final HTMLTextAreaElement textArea = (HTMLTextAreaElement) getDocument().getElementById("stepsTextArea");
+        textArea.value = "Hier sind einige Schritte, die du stattdessen unternehmen kannst:\n\n" +
+                "1. System?\n" +
+                "2. Benachrichtigung?\n" +
+                "3. Beweis?\n" +
+                "4. Was verbessern?\n";
+    }
+
+     void addButton() {
+        final HTMLButtonElement button = (HTMLButtonElement) getDocument().getElementById("helloButton");
+        button.addEventListener("click", (Event event) -> {
+            logger.info("Button clicked");
+            Window.alert("Hello World!");
         });
-	}
+    }
+    
+    HTMLDocument getDocument() {
+        return DomGlobal.document;
+    }
 }
--- viola-devk-client/src/test/java/com/github/lofidewanto/viodevk/client/AppEntryPointTest.java
+++ viola-devk-client/src/test/java/com/github/lofidewanto/viodevk/client/AppEntryPointTest.java
@@ -1,36 +1,37 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 package com.github.lofidewanto.viodevk.client;
 
-import static org.junit.Assert.assertTrue;
+import elemental2.dom.HTMLButtonElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import elemental2.dom.*;
 
-import com.google.gwtmockito.GwtMockitoTestRunner;
+import static org.mockito.Mockito.*;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-@RunWith(GwtMockitoTestRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class AppEntryPointTest {
 
-    @Test
-	public void test_ok() {
-        assertTrue(true);
+    @Mock(answer= Answers.RETURNS_DEEP_STUBS)
+    private HTMLDocument document;
+
+    @Mock(answer= Answers.RETURNS_DEEP_STUBS)
+    private HTMLButtonElement button;
+
+    @BeforeEach
+    public void setUp() {
     }
-    
+
+    @Test
+    public void addButton_created() {
+        AppEntryPoint appEntryPoint = spy(AppEntryPoint.class);
+        when(appEntryPoint.getDocument()).thenReturn(document);
+        when(appEntryPoint.getDocument().getElementById("helloButton")).thenReturn(button);
+
+        appEntryPoint.addButton();
+
+        verify(button, times(1)).addEventListener(eq("click"), any());
+    }
 }
