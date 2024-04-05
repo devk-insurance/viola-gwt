603a0231c3a48089fe48ab102be01871715a803c
Lofi Dewanto <blasiuslofi.dewanto@devk.de>
2024-04-05 22:09:27 +0200

    update some stuffs and reformat

--- viola-devk-client/src/main/java/com/github/lofidewanto/viodevk/client/AppEntryPoint.java
+++ viola-devk-client/src/main/java/com/github/lofidewanto/viodevk/client/AppEntryPoint.java
@@ -2,7 +2,10 @@ package com.github.lofidewanto.viodevk.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.Window;
-import elemental2.dom.*;
+import elemental2.dom.DomGlobal;
+import elemental2.dom.Event;
+import elemental2.dom.HTMLButtonElement;
+import elemental2.dom.HTMLTextAreaElement;
 
 import java.util.logging.Logger;
 
@@ -13,11 +16,12 @@ public class AppEntryPoint implements EntryPoint {
 
     @Override
     public void onModuleLoad() {
-        addButton();
+        addHelloButton();
         addTextArea();
+        addDeleteButton();
     }
 
-     void addTextArea() {
+    void addTextArea() {
         final HTMLTextAreaElement textArea = (HTMLTextAreaElement) DomGlobal.document.getElementById("stepsTextArea");
         textArea.value = "Hier sind einige Schritte, die du unternehmen kannst:\n\n" +
                 "1. System?\n" +
@@ -26,11 +30,23 @@ public class AppEntryPoint implements EntryPoint {
                 "4. Was verbessern?\n";
     }
 
-     void addButton() {
+    void addHelloButton() {
         final HTMLButtonElement button = (HTMLButtonElement) DomGlobal.document.getElementById("helloButton");
         button.addEventListener("click", (Event event) -> {
-            logger.info("Button clicked");
+            logger.info("Hello Button clicked");
             Window.alert("Hello World!");
         });
+    }
+
+    void addDeleteButton() {
+        final HTMLButtonElement button = (HTMLButtonElement) DomGlobal.document.getElementById("deleteButton");
+        button.addEventListener("click", (Event event) -> {
+            logger.info("Delete Button clicked");
+            makeSomethingFunny();
+        });
+    }
+
+    void makeSomethingFunny() {
+
     }
 }
--- viola-devk-client/src/test/java/com/github/lofidewanto/viodevk/client/AppEntryPointTest.java
+++ viola-devk-client/src/test/java/com/github/lofidewanto/viodevk/client/AppEntryPointTest.java
@@ -1,23 +1,23 @@
 package com.github.lofidewanto.viodevk.client;
 
 import elemental2.dom.HTMLButtonElement;
+import elemental2.dom.HTMLDocument;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Answers;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import elemental2.dom.*;
 
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class AppEntryPointTest {
 
-    @Mock(answer= Answers.RETURNS_DEEP_STUBS)
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
     private HTMLDocument document;
 
-    @Mock(answer= Answers.RETURNS_DEEP_STUBS)
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
     private HTMLButtonElement button;
 
     @BeforeEach
@@ -28,7 +28,7 @@ public class AppEntryPointTest {
     public void addButton_created() {
         AppEntryPoint appEntryPoint = spy(AppEntryPoint.class);
 
-        appEntryPoint.addButton();
+        appEntryPoint.addHelloButton();
 
         verify(button, times(1)).addEventListener(eq("click"), any());
     }
